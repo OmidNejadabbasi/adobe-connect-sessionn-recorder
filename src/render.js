30915ec2b4c7fbfa8b3e125c6c3ec1bd21bbc01b
Omid Nejadabbasi <nejadabbasio@gmail.com>
2021-10-17 22:42:18 +0330

    find elements in js and add eventlistener

--- src/render.js
+++ src/render.js
@@ -1,1 +1,8 @@
+const startRecordingBtn = document.querySelector('#start-recording');
 
+const urlInputField = document.querySelector('#session-url-input');
+
+function startRecording() {
+
+}
+startRecordingBtn.addEventListener('click', startRecording);
