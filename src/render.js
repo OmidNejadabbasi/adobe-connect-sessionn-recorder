be1bc239a1d291161901bd61cc35424b84806b6d
Omid Nejadabbasi <nejadabbasio@gmail.com>
2021-10-18 09:40:00 +0330

    open session window but needs Ruffle Plugin

--- src/render.js
+++ src/render.js
@@ -3,6 +3,15 @@ const startRecordingBtn = document.querySelector('#start-recording');
 const urlInputField = document.querySelector('#session-url-input');
 
 function startRecording() {
+    const electron = require('electron');
+    const BrowserWindow = electron.remote.BrowserWindow;
 
+    const sessionWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+    sessionWindow.loadURL("https://vc1.tabrizu.ac.ir/pcke6b05jhl6/?session=breezbreezdnvywsqh375wkaq2")
+    sessionWindow.setTitle("Recording session ...");
+    console.log('btnclicked')
 }
 startRecordingBtn.addEventListener('click', startRecording);
