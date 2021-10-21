d6363d2ebf8b886569f85b6069cd69ebcbf298c0
Omid Nejadabbasi <nejadabbasio@gmail.com>
2021-10-21 10:21:27 +0300

    play the recording but the timing of elements is not right

--- src/sessionPreload.js
+++ src/sessionPreload.js
@@ -1,12 +1,29 @@
+const openInBrowserXPath = '//*[@id="launchMeetingOptionsPage"]/div[8]/a';
+const playButtonXPath = '//*[@id="play-recording-shim-button"]';
+const timeProgressXPath = '//*[@id="playBar"]/div/div/div/div[5]'
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("proloader works!");
     // clicking the open in browser button
-    getElementByXpath('//*[@id="launchMeetingOptionsPage"]/div[8]/a').click();
+    getElementByXpath(openInBrowserXPath).click();
 
     // click the play button
     setTimeout(() => {
-        getElementByXpath('//*[@id="play-recording-shim-button"]').click();
-    }, 1000);
+        getElementByXpath(playButtonXPath).click();
+    }, 23000);
+
+    const progressTextElem = getElementByXpath(timeProgressXPath);
+    setTimeout(() => {
+        var observer = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                if (mutation.type === "characterData")
+                    console.log(mutation.target.textContent); // <- It always detects changes
+            });
+        });
+
+        var config = { characterData: true, subtree: true };
+        observer.observe(progressTextElem, config);
+    }, 23000);
 });
 
 function getElementByXpath(path) {
